c59ee44f7e907f1895a1d14f1e3228dc71c6238c
UvBeenZacked <bentziyaari@gmail.com>
2025-03-10 18:08:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function checkPassword() {
+    var password = document.getElementById("password").value;
+
+    // Change this password to your desired one
+    var correctPassword = "mypassword";
+
+    if (password === correctPassword) {
+        window.location.href = "protected.html"; // Redirect to the protected page
+    } else {
+        document.getElementById("error-message").innerText = "Incorrect password. Try again.";
+    }
+}
+
+
