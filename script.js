ad016364cdec703ae5dfb21700e90f4eeb1d05db
UvBeenZacked <bentziyaari@gmail.com>
2025-03-10 18:28:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ function checkPassword() {
 
     // Change this password to your desired one
     var correctPassword = "12345";
+    var admin password = "24245";
 
     if (password === correctPassword) {
         window.location.href = "protected.html"; // Redirect to the protected page
     } else {
         document.getElementById("error-message").innerText = "Incorrect password. Try again.";
     }
+    if (password === admin password) {window.location.href=https://www.google.com/search?q=href&oq=href&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIHCAEQABiABDIHCAIQABiABDIHCAMQABiABDIPCAQQLhgKGMcBGNEDGIAEMgcIBRAAGIAEMgcIBhAAGIAEMgcIBxAAGIAEMgcICBAAGIAEMgcICRAAGIAE0gEIMTE1MGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8;
+                                      
 }
 
 
